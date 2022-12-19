e50c9841610503dbd7e1297a7f6531aa58c00f39
Francisco Leonel Soriano Hernandez <Fransoriano37@live.com>
2022-12-19 09:55:45 +0100

    Cambio

--- galeria.php
+++ galeria.php
@@ -18,47 +18,52 @@
 <body>
 
 
-  <h1>EJEMPLO EJERCICIO. Galer&iacute;as de im&aacute;genes</h1>
-  <h2>Una selecci&oacute;n de flores.</h2>
+  <h1 style="color:red">EJEMPLO EJERCICIO. Galer&iacute;as de im&aacute;genes</h1>
+  <h2 >Una selecci&oacute;n de flores.</h2>
+  <h3>Examen Entornos 1er Trimestre</h3>
 
   
 
    
  
-      <img src="imagenes/1.jpg" alt="Flores de corte" width="200" height="200">
+      <img src="imagenes/1.jpg" alt="Flores de corte" width="200" height="200" >
     
-      Foto 1: Flores de corte   
+      <p>Foto 1: Flores de corte   </p>
      
     
       <img src="imagenes/2.jpg" alt="Mirabilis Jalapa" width="200" height="200">
      
-      Foto 2: Flor de Mirabilis Jalapa
+      <p>Foto 2: Flor de Mirabilis Jalapa </p>
      
     
       <img src="imagenes/3.jpg" alt="Strelitzia" width="200" height="200">
      
-     Foto3: Strelitzia. Flores de las aves.
+    <p> Foto3: Strelitzia. Flores de las aves.</p>
     
     
        <img src="imagenes/4.jpg" alt="Flor del opio" width="200" height="200">
       
-      Foto 4: Flor del opio.
+     <p> Foto 4: Flor del opio.</p>
       
    
        <img src="imagenes/5.jpg" alt="Rosas" width="200" height="200">
      
-      Foto 5: Rosas
+  </p>Foto 5: Rosas</p>
      
     
-      <img src="imagenes/6.jpg" alt="Pensamiento" width="200" height="200">
+      <img src="imagenes/6.jpg" alt="Pensamiento" width="200" height="200" >
      
-     Foto 6: Pensamiento
+    <p> Foto 6: Pensamiento</p>
     
      
 
   <p>Copyright 2022, www.cesurformaion.com</p>
 
 
-  
+  <h1 style="color:red">
+  <?php
+          echo 'Autor: (Francisco Leonel Soriano Hernandez)';
+          ?>
+  </h1>
 </body>
 </html>
